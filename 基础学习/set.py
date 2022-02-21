1439802c33765ab3033d31af5415cb98f07010fc
郑泽钦 <zhengzeqin@addcn.com>
2022-02-21 19:13:03 +0800

    <add>(py): set

---
+++ 基础学习/set.py
@@ -0,0 +1,14 @@
+# 字典
+def set():
+    set1 = set(['Java', 'OC', 'Swift', 'Swift'])
+    print(set1)
+    # set1.add('Dart')
+    # print(set1)
+    # set1.remove('Swift')
+    # print(set1)
+
+def main():
+    set()
+
+if __name__ == '__main__':
+    main()
