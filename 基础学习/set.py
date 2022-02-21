5044742b6ee95eea124ba477f4c3c968af3569d9
郑泽钦 <zhengzeqin@addcn.com>
2022-02-21 19:43:52 +0800

    <update>(py)：更新 set

--- 基础学习/set.py
+++ 基础学习/set.py
@@ -1,14 +1,25 @@
-# 字典
-def set():
+# set
+def setFunc():
     set1 = set(['Java', 'OC', 'Swift', 'Swift'])
     print(set1)
-    # set1.add('Dart')
-    # print(set1)
-    # set1.remove('Swift')
-    # print(set1)
+    set1.add('Dart')
+    print("添加 Dart:", set1)
+    set1.remove('Swift')
+    print("移除 Swift:", set1)
+    # 交集 (求两个 set 集合中相同的元素)
+    set2 = set(['Shell', 'Python', 'Ruby', 'Java'])
+    set3 = set1 & set2
+    print("求两个 set 集合中相同的元素:", set3)
+    # 并集 （合并两个 set 集合的元素并去除重复的值）
+    set4 = set1 | set2
+    print("合并两个 set 集合的元素并去除重复的值:", set4)
+    # 差集
+    set5 = set1 - set2
+    print("差集:", set5)
+
 
 def main():
-    set()
+    setFunc()
 
 if __name__ == '__main__':
     main()
