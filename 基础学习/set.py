743fb5279f908e1d9be48b1c4d745e596e5cf2a9
郑泽钦 <zhengzeqin@addcn.com>
2022-02-21 22:59:41 +0800

    <update>(py): 字典 & set

--- 基础学习/set.py
+++ 基础学习/set.py
@@ -6,17 +6,19 @@ def setFunc():
     print("添加 Dart:", set1)
     set1.remove('Swift')
     print("移除 Swift:", set1)
-    # 交集 (求两个 set 集合中相同的元素)
     set2 = set(['Shell', 'Python', 'Ruby', 'Java'])
+    # 交集 & : set1&set2，返回一个新的集合，包括同时在集合 set1 和 set2 中的共同元素。
     set3 = set1 & set2
-    print("求两个 set 集合中相同的元素:", set3)
-    # 并集 （合并两个 set 集合的元素并去除重复的值）
+    print("交集 求两个 set 集合中相同的元素:", set3)
+    # 并集 | : set1|set2，返回一个新的集合，包括集合 set1 和 set2 中所有元素。
     set4 = set1 | set2
-    print("合并两个 set 集合的元素并去除重复的值:", set4)
-    # 差集
+    print("并集 包括集合 set1 和 set2 中所有元素:", set4)
+    # 差集 - : set1-set2，返回一个新的集合,包括在集合 set1 中但不在集合 set2 中的元素
     set5 = set1 - set2
-    print("差集:", set5)
-
+    print("差集 包括在集合 set1 中但不在集合 set2 中的元素:", set5)
+    # 补集 ^ : set1^set2，返回一个新的集合，包括集合 set1 和 set2 的非共同元素。
+    set6 = set1 - set2
+    print("差集 集合 set1 和 set2 的非共同元素:", set6)
 
 def main():
     setFunc()
