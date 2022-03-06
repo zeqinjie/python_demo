e09e0bfa534076e51e29d470d3eccb7dcf696fab
郑泽钦 <zhengzeqin@addcn.com>
2022-03-06 22:16:40 +0800

    <add>(py)：模块使用

---
+++ 基础学习/module.py
@@ -0,0 +1,22 @@
+# 一般的导入模块的方式
+import set
+# 导入模块使用别名 as
+import iter_and_yield as my_yield
+# 导入模块的部分方法
+from filter_and_map import filter_func, map_func
+# 导入模块的所有内容
+from dict import *
+
+if __name__ == "__main__":
+    # 一般使用情况
+    set.set_func()
+
+    # 使用别名 as
+    my_yield.yield_func()
+
+    # 使用 from 方式
+    filter_func()
+    map_func()
+
+    # 导入模块的所有内容
+
