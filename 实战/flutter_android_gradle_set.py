acfd90d0be947628975a155f771d345398e3c02c
郑泽钦 <zhengzeqin@addcn.com>
2022-03-07 23:32:20 +0800

    <update>(py 实战)：优化脚本内容

--- 实战/flutter_android_gradle_set.py
+++ 实战/flutter_android_gradle_set.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# -*- author: zhengzeqin -*-
+# -*- date: 2022-03-07 -*-
 
 """
 Flutter 修改默认生成的 minSdkVersion 问题
@@ -9,18 +12,19 @@ py 解决思路: 通过脚本一键复制修改 flutter 配置模板
 """
 
 import os
+import sys
 import getopt
 
 # 定义 flutter 目标 android project build_gradle 配置的路径
-_project_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/gradle/build.gradle.tmpl"
+_project_build_gradle_set_file = "/Users/zhengzeqin/flutter/packages/flutter_tools/templates/module/android/gradle/build.gradle.tmpl"
 # 定义 flutter 目标 android app build_gradle 配置的路径
-_app_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/host_app_common/app.tmpl/build.gradle.tmpl"
+_app_build_gradle_set_file = "/Users/zhengzeqin/flutter/packages/flutter_tools/templates/module/android/host_app_common/app.tmpl/build.gradle.tmpl"
 
 # 定义需要写入的 android project 根目录的 build_gradle 配置
-_build_gradle_tmpl_for_project_path = "/build_gradle_tmpl_for_project.txt"
+_build_gradle_tmpl_for_project = "/build_gradle_tmpl_for_project.txt"
 
 # 定义需要写入的 android app 目录的 build_gradle 配置
-_build_gradle_tmpl_for_app_path = "/build_gradle_tmpl_for_app.txt"
+_build_gradle_tmpl_for_app = "/build_gradle_tmpl_for_app.txt"
 
 # 读取文件内容
 def readfile(file_name):
@@ -46,11 +50,11 @@ def android_gradle_set():
     write_file.close()
 
 # 从文件路径中写入到目标文件
-def set_build_gradle_file(read_file_path, write_file_path):
+def set_build_gradle_file(read_file_path, write_file):
     read_file = open(read_file_path, 'r')
-    write_file = open(write_file_path, "w")
+    write_file = open(write_file, "w")
     s = read_file.read()
-    w = write_file.write(s)
+    write_file.write(s)
     read_file.close()
     write_file.close()
 
@@ -61,22 +65,51 @@ def get_current_path():
 # 设置 app 的 file
 def set_app_build_gradle_file():
     concurrent = os.path.abspath(os.path.dirname(__file__))
-    read_file_path = concurrent + _build_gradle_tmpl_for_app_path
+    read_file_path = concurrent + _build_gradle_tmpl_for_app
     # check_path = concurrent + "/app"
     # write_file_path = concurrent + "/build.gradle.tmpl"
     # mkdir(check_path)
-    set_build_gradle_file(read_file_path, _app_build_gradle_set_file_path)
+    set_build_gradle_file(read_file_path, _app_build_gradle_set_file)
 
 # 设置 project 根目录的 file
 def set_pro_build_gradle_file():
     concurrent = os.path.abspath(os.path.dirname(__file__))
-    read_file_path = concurrent + _build_gradle_tmpl_for_project_path
-    # write_file_path = concurrent + "/build.gradle.tmpl"
-    set_build_gradle_file(read_file_path, _project_build_gradle_set_file_path)
+    read_file_path = concurrent + _build_gradle_tmpl_for_project
+    set_build_gradle_file(read_file_path, _project_build_gradle_set_file)
 
-
-if __name__ == "__main__":
-    # 修改 Android 项目的 build_gradle 项目
+# 通过本地文件配置路径修改 Android 项目的 build_gradle 配置
+def set_android_build_gradle_params_files():
     set_pro_build_gradle_file()
     set_app_build_gradle_file()
 
+# 通过参数配置路径修改 Android 项目的 build_gradle 配置
+def set_android_build_gradle_argv_files():
+    argv = sys.argv[1:]
+    # android project build_gradle 配置的路径
+    project_path = ""
+    # android app build_gradle 配置的路径
+    app_path = ""
+    # 默认关闭
+    is_close_bitcode = True
+    try:
+        opts, args = getopt.getopt(argv, "p:a:", ["project=", "app="])
+    except getopt.GetoptError:
+        print('flutter_android_gradle_set.py -p "项目路径"')
+        sys.exit(2)
+
+    print("opts ===>", opts)
+
+    for opt, arg in opts:
+        if opt in ["-p", "--project"]:
+            project_path = arg
+        if opt in ["-a", "--app"]:
+            app_path = arg
+    # 设置 project build gradle
+    set_build_gradle_file(project_path, _project_build_gradle_set_file)
+    # 设置 app build gradle
+    set_build_gradle_file(app_path, _project_build_gradle_set_file)
+
+if __name__ == "__main__":
+    set_android_build_gradle_params_files()
+    # set_android_build_gradle_argv_files()
+    pass
