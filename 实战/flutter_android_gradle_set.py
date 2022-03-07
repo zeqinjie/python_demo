feb1a54dc9e237a89fad12fed8a88fad4cf69113
郑泽钦 <zhengzeqin@addcn.com>
2022-03-07 18:58:34 +0800

    <add>(py实战)：测试脚本

--- 实战/flutter_android_gradle_set.py
+++ 实战/flutter_android_gradle_set.py
@@ -11,15 +11,15 @@ py 解决思路: 通过脚本一键复制修改 flutter 配置模板
 import os
 import getopt
 
-# 定义项目根目录下的 build_gradle 配置
-_project_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/gradle"
-# 定义项目 app 目录下的 build_gradle 配置
-_app_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/host_app_common/app.tmpl"
+# 定义 flutter 目标 android project build_gradle 配置的路径
+_project_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/gradle/build.gradle.tmpl"
+# 定义 flutter 目标 android app build_gradle 配置的路径
+_app_build_gradle_set_file_path = "/Users/addcn/flutter/packages/flutter_tools/templates/module/android/host_app_common/app.tmpl/build.gradle.tmpl"
 
-# 定义需要写入的 android 项目根目录的 build_gradle 配置
+# 定义需要写入的 android project 根目录的 build_gradle 配置
 _build_gradle_tmpl_for_project_path = "/build_gradle_tmpl_for_project.txt"
 
-# 定义需要写入的 app 目录的 build_gradle 配置
+# 定义需要写入的 android app 目录的 build_gradle 配置
 _build_gradle_tmpl_for_app_path = "/build_gradle_tmpl_for_app.txt"
 
 # 读取文件内容
@@ -63,16 +63,16 @@ def set_app_build_gradle_file():
     concurrent = os.path.abspath(os.path.dirname(__file__))
     read_file_path = concurrent + _build_gradle_tmpl_for_app_path
     # check_path = concurrent + "/app"
-    write_file_path = concurrent + "/build.gradle.tmpl"
+    # write_file_path = concurrent + "/build.gradle.tmpl"
     # mkdir(check_path)
-    set_build_gradle_file(read_file_path, write_file_path)
+    set_build_gradle_file(read_file_path, _app_build_gradle_set_file_path)
 
 # 设置 project 根目录的 file
 def set_pro_build_gradle_file():
     concurrent = os.path.abspath(os.path.dirname(__file__))
     read_file_path = concurrent + _build_gradle_tmpl_for_project_path
-    write_file_path = concurrent + "/build.gradle.tmpl"
-    set_build_gradle_file(read_file_path, write_file_path)
+    # write_file_path = concurrent + "/build.gradle.tmpl"
+    set_build_gradle_file(read_file_path, _project_build_gradle_set_file_path)
 
 
 if __name__ == "__main__":
