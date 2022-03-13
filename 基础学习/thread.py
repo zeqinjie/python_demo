5c3c15affe9c196b43ea7133a67e4d2b47b4d99f
郑泽钦 <zhengzeqin@addcn.com>
2022-03-13 15:07:06 +0800

    <add>(py 基础)：多线程 & 锁

--- 基础学习/thread.py
+++ 基础学习/thread.py
@@ -2,15 +2,36 @@ import threading
 import time
 from threading import current_thread
 
-def myThread(arg1, arg2):
+
+# 定义线程方法
+def my_thread(arg1, arg2):
     print(threading.currentThread().getName(), 'start')
-    print('%s %s'%(arg1, arg2))
+    print('%s %s' % (arg1, arg2))
     time.sleep(1)
     print(threading.currentThread().getName(), 'end')
 
-for i in range(1,6):
-    t1 = threading.Thread(target=myThread, args=(i, i+1))
+
+# 测试
+def test_func():
+    for i in range(1, 6):
+        t1 = threading.Thread(target=my_thread, args=(i, i + 1))
+        t1.start()
+    print(current_thread().getName(), 'end')
+
+
+class MyThread(threading.Thread):
+    def run(self):
+        print(current_thread().getName(), 'start')
+        print('run')
+        print(current_thread().getName(), 'stop')
+
+
+def test_func2():
+    t1 = MyThread()
     t1.start()
+    t1.join()
+    print(current_thread().getName(), 'end')
 
 
-print(current_thread().getName(), 'end')
+# test_func()
+test_func2()
